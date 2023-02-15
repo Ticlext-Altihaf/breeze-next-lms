78011b0ca162d3fef34826aadfdf7718a91b8983
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2023-02-15 15:48:24 +0700

    Markdown everyhwere

--- src/pages/courses/[idCourse]/lessons/[idLesson]/edit/[page].js
+++ src/pages/courses/[idCourse]/lessons/[idLesson]/edit/[page].js
@@ -10,6 +10,8 @@ import Link from 'next/link'
 import AppLayout from '@/components/Layouts/AppLayout'
 import Editor from '@/components/Markdown/Editor'
 import Preview from '@/components/Markdown/Preview'
+import Dropdown from '@/components/Dropdown'
+import { DropdownButton } from '@/components/DropdownLink'
 export default function EditContent() {
     const router = useRouter()
     const { page, idLesson, idCourse } = router.query
@@ -57,10 +59,40 @@ export default function EditContent() {
                 header={
                     <h2 className="font-semibold text-xl leading-tight flex justify-between">
                         Giga
+                        {/* Settings Dropdown */}
+                        <div className="hidden sm:flex sm:items-center sm:ml-6">
+                            <Dropdown
+                                align="right"
+                                width="48"
+                                trigger={
+
+
+                                    <button className="flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 focus:outline-none transition duration-150 ease-in-out">
+                                        <div>Menu</div>
+                                        <div className="ml-1">
+                                            <svg
+                                                className="fill-current h-4 w-4"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                viewBox="0 0 20 20">
+                                                <path
+                                                    fillRule="evenodd"
+                                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                    clipRule="evenodd"
+                                                />
+                                            </svg>
+                                        </div>
+                                    </button>
+                                }>
+                                <DropdownButton>Save</DropdownButton>
+                                <DropdownButton>Delete</DropdownButton>
+                                <DropdownButton>Exit</DropdownButton>
+                            </Dropdown>
+                        </div>
                     </h2>
                 }>
                 <Head>
                     <title>Laravel - Dashboard</title>
+
                 </Head>
                 <div>
                     <main
