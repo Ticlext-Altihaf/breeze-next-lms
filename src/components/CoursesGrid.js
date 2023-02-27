8f7304d2abd76f9aad280afd5899a6140f6bde2a
Ticlext-Altihaf <yusufsekhanaltaf@smknegeri1garut.sch.id>
2023-02-27 15:21:14 +0700

    border

--- src/components/CoursesGrid.js
+++ src/components/CoursesGrid.js
@@ -1,35 +1,61 @@
+import Image from 'next/image'
+
 const CoursesGrid = ({ courses }) => {
     return (
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5">
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-5  text-gray-800 dark:text-gray-100">
             {courses.map(course => (
-                <a href={`/courses/${course.id}`} key={course.id}>
-                    <div className="max-w-sm rounded overflow-hidden shadow-lg bg-white dark:bg-gray-800">
-                        {course.image_url && (
+                <div className="w-full p-4" key={course.id}>
+                    <a
+                        href=""
+                        className="c-card block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden dark:bg-gray-800">
+                        <div className="relative pb-48 overflow-hidden">
                             <img
-                                className="w-full"
+                                className="absolute inset-0 h-full w-full object-cover"
                                 src={course.image_url}
-                                alt={course.name}
+                                alt=""
                             />
-                        )}
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2 text-gray-700 dark:text-gray-200">
+                        </div>
+                        <div className="p-4 border-b">
+                            <span
+                                className="inline-block px-2 py-1 leading-none bg-orange-200 text-orange-800 rounded-full font-semibold uppercase tracking-wide text-xs"
+                                style={{ display: 'none' }}>
+                                Highlight
+                            </span>
+                            <h2 className="mt-2 mb-2 text-lg font-bold">
                                 {course.name}
+                            </h2>
+                            <p className="text-sm">{course.description}</p>
+                        </div>
+                        {course.categories.length > 0 && (
+                            <div className="p-4 border-b text-xs">
+                                {course.categories.map(category => (
+                                    <span
+                                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 dark:bg-gray-700 dark:text-gray-200"
+                                        key={category.id}>
+                                        {category.name}
+                                    </span>
+                                ))}
                             </div>
-                            <p className="text-gray-700 text-base dark:text-gray-200">
-                                {course.description}
-                            </p>
+                        )}
+                        <div className="p-4 flex items-center text-sm">
+                            <div className="flex items-center">
+                                <img
+                                    className="w-8 h-8 rounded-full mr-4"
+                                    src={
+                                        course.author?.image_url ||
+                                        'https://loremflickr.com/500/500/face?random=EM1OvbuF5lYbokyA'
+                                    }
+                                    alt="Avatar of Author"
+                                />
+                                <div className="text-left">
+                                    <p className="text-black dark:text-white">
+                                        {course.author.name}
+                                    </p>
+                                </div>
+                            </div>
                         </div>
-                        <div className="px-6 pt-4 pb-2">
-                            {course.categories.map(category => (
-                                <span
-                                    className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2 dark:bg-gray-700 dark:text-gray-200"
-                                    key={category.id}>
-                                    {category.name}
-                                </span>
-                            ))}
-                        </div>
-                    </div>
-                </a>
+                    </a>
+                </div>
             ))}
         </div>
     )
